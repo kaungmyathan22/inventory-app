e7ffb2540a320d7bf95802e9f8f891c563b48c3c
Kaung Myat Han <arkarhtethan@gmail.com>
2023-07-21 19:40:23 +0630

    refactor(): reusable button component created

---
+++ frontend/src/shared/PrimaryButton.tsx
@@ -0,0 +1,22 @@
+import { ButtonHTMLAttributes } from "react";
+import IfElse from "./IfElse";
+import { Loader } from "./Loader";
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
+}
+
+export function PrimaryButton({
+  children,
+  isLoading = false,
+  ...rest
+}: IButtonProps) {
+  return (
+    <button
+      className="flex justify-center w-full px-5 py-2 text-white rounded-lg bg-primary-600"
+      {...rest}
+    >
+      <IfElse isTrue={isLoading} ifBlock={<Loader />} elseBlock={children} />
+    </button>
+  );
+}
