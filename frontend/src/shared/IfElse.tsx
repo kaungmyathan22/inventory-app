3a064ba81ce8076efa7684a14a2e887f5c09c853
Kaung Myat Han <arkarhtethan@gmail.com>
2023-07-21 19:40:36 +0630

    chore(): reusable if/else component created

---
+++ frontend/src/shared/IfElse.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  isTrue: boolean;
+  ifBlock: React.ReactNode;
+  elseBlock: React.ReactNode;
+}
+
+const IfElse = ({ isTrue, ifBlock, elseBlock }: Props) => {
+  return <>{isTrue ? <>{ifBlock}</> : <>{elseBlock}</>}</>;
+};
+
+export default IfElse;
