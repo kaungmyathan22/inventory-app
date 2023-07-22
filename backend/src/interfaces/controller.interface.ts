2b0a609e08529f206a020953314457c72366e001
Kaung Myat Han <arkarhtethan@gmail.com>
2023-07-22 09:56:18 +0630

    feat(): interfaces added

---
+++ backend/src/interfaces/controller.interface.ts
@@ -0,0 +1,6 @@
+import * as express from "express";
+
+export class Controller {
+    route:string;
+    router:express.Router;
+}
---
+++ backend/src/interfaces/pagination.interface.ts
@@ -0,0 +1,5 @@
+export interface IPagination {
+  page?: number;
+  rowsPerPage?: number;
+  searchKeywords?: string;
+}
